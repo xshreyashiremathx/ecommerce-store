cf65d924b139b4741074ce450a407b6e5c54195e
xshreyashiremathx <shreyyhiremath@gmail.com>
2023-07-31 20:45:29 +0530

    Initial commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,28 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("e7bff8ed-b45d-4f1c-bfc6-e4595aa9c032");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
